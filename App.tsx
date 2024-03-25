fcca63374d0bb909787f59cd6b8d6ec40abfadda
cucumber5252 <kangdonghyuk@gangdonghyeog-ui-noteubug.local>
2024-03-25 21:24:29 +0900

    FEAT: FCM Android&ios Development Envrionment Setting

--- App.tsx
+++ App.tsx
@@ -4,28 +4,40 @@ import {WebView, WebViewNavigation} from 'react-native-webview';
 import messaging from '@react-native-firebase/messaging';
 
 const App = () => {
-  // useEffect(() => {
-  //   getFcmToken();
-  //   subscribe();
-  //   return () => {
-  //     subscribe();
-  //   };
-  // }, []);
+  useEffect(() => {
+    getFcmToken();
 
-  // /**
-  //  * FCM 토큰을 받습니다.
-  //  */
-  // const getFcmToken = async () => {
-  //   const fcmToken = await messaging().getToken();
-  //   console.log('[+] FCM Token :: ', fcmToken);
-  // };
+    // 구독하고, 구독 해제 함수를 반환받습니다.
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      console.log('[+] Remote Message ', JSON.stringify(remoteMessage));
+    });
 
-  // /**
-  //  * FCM 메시지를 앱이 foreground 상태일 경우 메시지를 수신합니다.
-  //  */
-  // const subscribe = messaging().onMessage(async remoteMessage => {
-  //   console.log('[+] Remote Message ', JSON.stringify(remoteMessage));
-  // });
+    // 컴포넌트가 언마운트될 때 구독을 취소합니다.
+    return unsubscribe;
+  }, []);
+
+  /**
+   * FCM 토큰을 받습니다.
+   */
+  const getFcmToken = async () => {
+    try {
+      const fcmToken = await messaging().getToken();
+      if (fcmToken) {
+        console.log('[+] FCM Token :: ', fcmToken);
+      } else {
+        console.log('FCM Token을 받지 못했습니다.');
+      }
+    } catch (error) {
+      console.log('FCM 토큰을 받는 데 실패했습니다.', error);
+    }
+  };
+
+  /**
+   * FCM 메시지를 앱이 foreground 상태일 경우 메시지를 수신합니다.
+   */
+  const subscribe = messaging().onMessage(async remoteMessage => {
+    console.log('[+] Remote Message ', JSON.stringify(remoteMessage));
+  });
 
   // 커스텀 URL 스키마를 처리하는 함수
   const handleShouldStartLoadWithRequest = (request: WebViewNavigation) => {
@@ -46,8 +58,8 @@ const App = () => {
   return (
     <SafeAreaView style={styles.flexContainer}>
       <WebView
-        source={{uri: 'http://localhost:3000/'}} //ios용
-        // source={{uri: 'http://10.0.2.2:3000/'}} //android용
+        // source={{uri: 'http://localhost:3000/'}} //ios용
+        source={{uri: 'http://10.0.2.2:3000/'}} //android용
         onShouldStartLoadWithRequest={handleShouldStartLoadWithRequest} // iOS에서 사용
         shouldOverrideUrlLoading={handleShouldStartLoadWithRequest} // Android에서 사용
       />
