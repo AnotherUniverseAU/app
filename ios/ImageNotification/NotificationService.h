ded43e0c135fb4552b823b51c32e915ff24a5a08
cucumber5252 <kangdonghyuk@gangdonghyeog-ui-noteubug.local>
2024-04-02 14:51:05 +0900

    FEAT: iOS Push Notification

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by 강동혁 on 4/2/24.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
